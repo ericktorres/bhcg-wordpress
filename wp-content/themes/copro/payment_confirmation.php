76750ae428faf51fd17d10a6aad961547b2b8bb7
Erick López Torres <ericktorres87@gmail.com>
2019-03-13 01:24:45 -0600

    Developing functionality - Conekta payment

--- wp-content/themes/copro/payment_confirmation.php
+++ wp-content/themes/copro/payment_confirmation.php
@@ -65,16 +65,61 @@ get_header(); ?>
 
 	<!-- Here start the payment confirmation page -->
 	<?php
+		//Variables
+		$course_name = $_POST['txt_course_name'];
+		$price = ($_POST['txt_course_cost'] * 100);
+		$client_name = $_POST['txt_name'] .' '. $_POST['txt_lastname'];
+		$email = $_POST['txt_confirm_email'];
+		$phone_number = $_POST['txt_mobile_phone'];
+		$course_code = $_POST['txt_course_code'];
+		$token_id = $_POST['conektaTokenId'];
 		//require_once("lib/Conekta.php");
 		\Conekta\Conekta::setApiKey("key_tSD9hmj2QAnLj5X7JxqGNg");
-		\Conekta\Conekta::setApiVersion("4.0.4");
+		\Conekta\Conekta::setApiVersion("2.0.0");
         
+        try{
+  			$order = \Conekta\Order::create(
+    			array(
+      				"line_items" => array(
+        				array(
+          					"name" => $course_name,
+          					"unit_price" => $price,
+          					"quantity" => 1
+        				)//first line_item
+      				), //line_items
+      				"currency" => "MXN",
+      				"customer_info" => array(
+        				"name" => $client_name,
+        				"email" => $email,
+        				"phone" => $phone_number
+      				), //customer_info
+      				"metadata" => array("reference" => $course_code, "more_info" => "Pago de curso en línea"),
+      				"charges" => array(
+          				array(
+              				"payment_method" => array(
+                  				//"monthly_installments" => 3,
+                  				"type" => "card",
+                  				"token_id" => $token_id
+                  			) //payment_method - use customer's default - a card
+                			//to charge a card, different from the default,
+                			//you can indicate the card's source_id as shown in the Retry Card Section
+          				) //first charge
+      				) //charges
+    			)//order
+  			);
+		}catch(\Conekta\ProcessingError $error){
+  			echo $error->getMessage();
+		}catch(\Conekta\ParameterValidationError $error){
+  			echo $error->getMessage();
+		}catch(\Conekta\Handler $error){
+  			echo $error->getMessage();
+		}
     ?>
 	<div class="container">
 		<div id="payment_status">
-			<?php
-			print_r($_POST);
-			?>
+			<?php if($order->payment_status == 'paid'){ ?>
+				<h1>Felicidades</h1>
+			<?php } ?>
 		</div>
 	</div>
 	
